93f18ff845662c7d91c414f96d6a6b01390a30fb
Rasulwebs <boboqulovrasul80@gamil.com>
2022-08-19 21:02:03 +0500

    javascripmasalaa

---
+++ stjss/stjss.js
@@ -0,0 +1,219 @@
+"user strict"
+
+// ==== 1. masala=======
+
+// let x =+prompt("sonni kiriting");
+
+// function sonKubi(x){
+//     // return x *x *x
+//     return Math.pow(x, 3)
+// }
+
+// alert(sonKubi(x))
+
+
+// ======= 2.masala ========
+
+// let a=+prompt(" a  son");
+// let b=+prompt("b son");
+
+// function yigindi(a,b){
+//     return a+b
+// }
+// alert(yigindi(a+b))
+
+// ======== 3.masala =========
+
+// let a=+prompt(" a  son");
+// let b=+prompt("b son");
+
+// function ayirma(a,b){
+//     return a-b
+// }
+// alert(ayirma(a-b))
+
+
+//  ========4,masala ==========
+
+// let a =+prompt("a sonni kiriting");
+
+// function darajalar2(a){
+//     return a*a
+
+// }
+// console.log(darajalar2(a));
+
+// function darajalar3(a){
+//     return Math.pow(a, 3)
+
+// }
+// console.log(darajalar3(a));
+
+
+// function darajalar4(a){
+//     return Math.pow(a, 4)
+
+// }
+
+// console.log(darajalar4(a));
+
+// ======== 5.masala =========
+
+// let a=+prompt("sonni kiriting");
+
+// function toqlar(a){
+//     for(let i=1; i<=a; i++){
+//       i % 2 == 1  ? console.log(i) : console.log();
+//     }
+// }
+// console.log(toqlar(a));
+
+// ======== 6.masala ==========
+
+// let a=+prompt("sonni kiriting");
+
+// function toqlar(a){
+//     for(let i=1; i<=a; i++){
+//       i % 2 == 1  ? console.log() : console.log(i);
+//     }
+// }
+// console.log(toqlar(a));
+
+// ========= 7.masala========
+
+// let n=+prompt("sonni kiriting");
+
+// function inkrement(n){
+//     return n-1
+// }
+// console.log(inkrement(n));
+
+// let n=+prompt("sonni kiriting");
+
+// function dikrement(n){
+//     return n+1
+// }
+// console.log(dikrement(n));
+
+
+// dikrement boashqacha yoli
+
+// let n=+prompt("sonni kiriting");
+// function krement(inkrement, dikrement){
+//     let inkre= ++inkrement;
+//     let dik = --dikrement;
+
+//     return `${inkre}, ${dik}`
+// }
+
+// console.log(krement(n,n));
+
+
+// ======= 8.masala==========
+
+// let n = +prompt("sonni tanlang");
+
+// function son(n) {
+//     for (let i = 1; i <= n; i++) {
+//         n % i == 0 ? console.log(i) : console.log();
+//     }
+
+// }
+// son(n)
+
+// ========= 9.masala =========
+
+// const result=document.querySelector('#result');
+// const num1=document.getElementById('numb1');
+// const num2=document.getElementById('numb2');
+
+// const add=()=>{
+//     return result.textContent=num1.value*1+num2.value*1;
+// }
+
+// const minus=()=>{
+//     return result.textContent=num1.value*1-num2.value*1;
+// }
+
+// const multiple=()=>{
+//     return result.textContent=num1.value*1 * num2.value*1;
+// }
+
+// const devide=()=>{
+//     return result.textContent=num1.value*1 / num2.value*1;
+// }
+
+
+
+// const cll=()=>{
+//     return (
+//         result.textContent="00",
+//         num1.value="",
+//         num2.value=""
+//     )
+// }
+
+
+// ======== 10.masala==========
+
+// function karra(){
+//     for(let i=1; i<=10; i++){
+//         for(let n=1; n<=10; n++){
+//             console.log(i+"x"+n+"="+i*n);
+//         }
+        
+//     }
+    
+
+// }
+// karra()
+
+
+
+
+// ======== qo'shimcha masala ==========
+// piyoda=3.6
+// velosiped=20.1
+// mashina=70
+// somalyot=800
+
+// let pp=3.6;
+// let vv=20.1;
+// let car=70;
+// let air=800;
+// let n=document.getElementById('mm');
+// let result=document.getElementById('result');
+// let res2 =document.getElementById('resultt');
+// let res3 =document.getElementById('resulttd');
+// let res4 =document.getElementById('resulttdt')
+
+
+
+//  const hisob=()=>{
+//         return (
+//             result.textContent=Math.round(n.value*1 / pp),
+//             res2.textContent=Math.round(n.value*1 / vv),
+//             res3.textContent=Math.round(n.value*1 / car),
+//             res4.textContent=Math.round(n.value*1 / air)
+
+
+        
+           
+//         )
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
